fd9ca478ee74128800d26b8ba9e98ad2fa12f0b0
maccallum <john.m@ccallum.com>
2021-11-29 22:58:13 +0100

    hyperslab message

--- help/h5.file.maxhelp
+++ help/h5.file.maxhelp
@@ -4,13 +4,13 @@
 		"appversion" : 		{
 			"major" : 8,
 			"minor" : 2,
-			"revision" : 0,
+			"revision" : 1,
 			"architecture" : "x64",
 			"modernui" : 1
 		}
 ,
 		"classnamespace" : "box",
-		"rect" : [ 565.0, 122.0, 640.0, 480.0 ],
+		"rect" : [ 565.0, 122.0, 997.0, 422.0 ],
 		"bglocked" : 0,
 		"openinpresentation" : 0,
 		"default_fontsize" : 12.0,
@@ -40,25 +40,164 @@
 		"assistshowspatchername" : 0,
 		"boxes" : [ 			{
 				"box" : 				{
-					"id" : "obj-6",
+					"id" : "obj-2",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 298.0, 321.0, 32.0, 22.0 ],
+					"text" : "print"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-30",
+					"linecount" : 2,
+					"maxclass" : "comment",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 688.0, 170.0, 263.0, 33.0 ],
+					"text" : "gethyperslab <dataset name> <dim0 offest> <dim0 count> ... <dimN offset> <dimN count>"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-29",
+					"maxclass" : "comment",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 44.0, 321.0, 172.0, 20.0 ],
+					"text" : "bang when done opening a file"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-27",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 228.0, 321.0, 32.0, 22.0 ],
+					"text" : "print"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-23",
+					"linecount" : 4,
+					"maxclass" : "comment",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 344.0, 64.0, 263.0, 60.0 ],
+					"text" : "for each dataset, output the following message:\n\ndataset <name> <data type> <data type order> <data type size> <rank> <dim0> ... <dimN>"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-22",
 					"maxclass" : "button",
 					"numinlets" : 1,
 					"numoutlets" : 1,
 					"outlettype" : [ "bang" ],
 					"parameter_enable" : 0,
-					"patching_rect" : [ 107.0, 140.0, 24.0, 24.0 ]
+					"patching_rect" : [ 250.0, 243.0, 24.0, 24.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-12",
+					"maxclass" : "comment",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 285.0, 243.0, 172.0, 20.0 ],
+					"text" : "bang when done opening a file"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-11",
+					"maxclass" : "comment",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 111.0, 24.0, 150.0, 20.0 ],
+					"text" : "open a file"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-21",
+					"maxclass" : "number",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "", "bang" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 673.0, 64.0, 50.0, 22.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-13",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 673.0, 96.0, 214.0, 22.0 ],
+					"text" : "gethyperslab Temperature 0 1 0 1 $1 1"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-4",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 673.0, 132.0, 208.0, 22.0 ],
+					"text" : "gethyperslab Temperature 0 5 0 5 0 5"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-9",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "bang", "" ],
+					"patching_rect" : [ 291.5, 98.0, 29.5, 22.0 ],
+					"text" : "t b l"
 				}
 
 			}
 , 			{
 				"box" : 				{
 					"id" : "obj-5",
-					"maxclass" : "button",
-					"numinlets" : 1,
+					"maxclass" : "newobj",
+					"numinlets" : 3,
+					"numoutlets" : 3,
+					"outlettype" : [ "", "", "" ],
+					"patching_rect" : [ 228.0, 279.0, 159.0, 22.0 ],
+					"text" : "routepass dataset hyperslab"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-6",
+					"maxclass" : "message",
+					"numinlets" : 2,
 					"numoutlets" : 1,
-					"outlettype" : [ "bang" ],
-					"parameter_enable" : 0,
-					"patching_rect" : [ 185.0, 55.0, 24.0, 24.0 ]
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 291.5, 64.0, 29.5, 22.0 ],
+					"text" : "info"
 				}
 
 			}
@@ -69,7 +208,7 @@
 					"numinlets" : 2,
 					"numoutlets" : 1,
 					"outlettype" : [ "" ],
-					"patching_rect" : [ 107.0, 55.0, 35.0, 22.0 ],
+					"patching_rect" : [ 69.0, 24.0, 35.0, 22.0 ],
 					"text" : "open"
 				}
 
@@ -79,9 +218,9 @@
 					"id" : "obj-1",
 					"maxclass" : "newobj",
 					"numinlets" : 1,
-					"numoutlets" : 1,
-					"outlettype" : [ "bang" ],
-					"patching_rect" : [ 107.0, 105.0, 41.0, 22.0 ],
+					"numoutlets" : 2,
+					"outlettype" : [ "", "" ],
+					"patching_rect" : [ 228.0, 214.0, 41.0, 22.0 ],
 					"text" : "h5.file"
 				}
 
@@ -89,8 +228,29 @@
  ],
 		"lines" : [ 			{
 				"patchline" : 				{
-					"destination" : [ "obj-6", 0 ],
+					"destination" : [ "obj-22", 0 ],
+					"source" : [ "obj-1", 1 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-5", 0 ],
 					"source" : [ "obj-1", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"source" : [ "obj-13", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-13", 0 ],
+					"source" : [ "obj-21", 0 ]
 				}
 
 			}
@@ -104,7 +264,35 @@
 , 			{
 				"patchline" : 				{
 					"destination" : [ "obj-1", 0 ],
+					"source" : [ "obj-4", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-2", 0 ],
+					"source" : [ "obj-5", 1 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-27", 0 ],
 					"source" : [ "obj-5", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-9", 0 ],
+					"source" : [ "obj-6", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"source" : [ "obj-9", 1 ]
 				}
 
 			}
--- source/h5.file.c
+++ source/h5.file.c
@@ -6,47 +6,341 @@
 #include "ext_path.h"
 #include "hdf5.h"
 
-#define H5FILE_OUTLET_DONEOPENING 0
+#define H5FILE_OUTLET_MAIN 0
+#define H5FILE_OUTLET_INFO 1
 
 void *h5file_class;
 
 typedef struct _h5file
 {
 	t_object ob;
-    void *outlets[1];
+    void *outlets[2];
     t_symbol *filename;
     t_symbol *filepath;
     hid_t fileid;
 } h5file;
 
-t_symbol *ps_emptystring;
+t_symbol *ps_emptystring, *ps_info, *ps_dataset, *ps_group,
+    *ps_floatingpoint, *ps_orderle, *ps_orderbe, *ps_hyperslab;
 
-herr_t h5file_opfunc(hid_t loc_id,
-                     const char *name,
-                     const H5L_info_t *info,
-                     void *operator_data)
+static void h5file_getHyperslab(h5file *x,
+                                t_symbol *s,
+                                long ac,
+                                t_atom *av)
 {
+    /* 
+       gethyperslab Temperature 0 10 0 10 0 10
+    */
+    hid_t file = x->fileid;
+    if(file <= 0)
+    {
+        object_error((t_object *)x, "no file open");
+        return;
+    }
+    if(ac < 3)
+    {
+        object_error((t_object *)x,
+                     "requires at least three arguments: "
+                     "name of the dataspace, dim0 offset, dim0 count, "
+                     "... <dimN offset> <dimN count>");
+        return;
+    }
+    if(atom_gettype(av) != A_SYM)
+    {
+        object_error((t_object *)x,
+                     "first argument must be a symbol "
+                     "(the name of the dataspace)");
+        return;
+    }
+    t_symbol *name = atom_getsym(av);
+
+    hid_t dataset = H5Dopen2(file, name->s_name, H5P_DEFAULT);
+    if(dataset < 0)
+    {
+        object_error((t_object *)x,
+                     "couldn't open dataset %s",
+                     name->s_name);
+        return;
+    }
+    hid_t datatype = H5Dget_type(dataset);
+    if(datatype < 0)
+    {
+        object_error((t_object *)x,
+                     "couldn't open datatype for dataset %s",
+                     name->s_name);
+        H5Sclose(dataset);
+        return;
+    }
+    H5T_class_t class = H5Tget_class(datatype);
+    H5T_order_t order = H5Tget_order(datatype);
+    size_t size = H5Tget_size(datatype);
+
+    hid_t dataspace = H5Dget_space(dataset);
+    if(dataspace < 0)
+    {
+        object_error((t_object *)x,
+                     "couldn't open dataspace for dataset %s",
+                     name->s_name);
+        H5Sclose(datatype);
+        H5Sclose(dataset);
+        return;
+    }
+    int rank = H5Sget_simple_extent_ndims(dataspace);
+    if(rank != ((ac - 1) / 2))
+    {
+        object_error((t_object *)x,
+                     "expected %d args, but got %d",
+                     rank + 1, ac);
+        H5Sclose(dataspace);
+        H5Sclose(datatype);
+        H5Sclose(dataset);
+        return;
+    }
+    hsize_t indims[rank];
+    int status_n = H5Sget_simple_extent_dims(dataspace, indims, NULL);
+    hsize_t offsets[rank];
+    hsize_t counts[rank];
+    hsize_t n = 1;
+    for(int i = 0; i < rank; i++)
+    {
+        long offset = atom_getlong(av + 1 + (i * 2));
+        long count = atom_getlong(av + 1 + (i * 2 + 1));
+        n *= count;
+        if(offset + count >= indims[i])
+        {
+            object_error((t_object *)x,
+                         "index for dimension %d out of bounds:"
+                         "%d + %d >= %d",
+                         i, offset, count, indims[i]);
+            H5Sclose(dataspace);
+            H5Sclose(datatype);
+            H5Sclose(dataset);
+            return;
+        }
+        offsets[i] = offset;
+        counts[i] = count;
+    }
+    herr_t status = H5Sselect_hyperslab(dataspace,
+                                        H5S_SELECT_SET,
+                                        offsets, NULL,
+                                        counts, NULL);
+    if(status < 0)
+    {
+        object_error((t_object *)x, "error selecting hyperslab");
+        H5Sclose(dataspace);
+        H5Sclose(datatype);
+        H5Sclose(dataset);
+    }
+    hid_t memspace = H5Screate_simple(rank, counts, NULL);
+    if(memspace < 0)
+    {
+        object_error((t_object *)x, "error creating memspace");
+        H5Sclose(dataspace);
+        H5Sclose(datatype);
+        H5Sclose(dataset);
+    }
+    hsize_t offsets_out[rank];
+    memset(offsets_out, 0, rank * sizeof(hsize_t));
+    status = H5Sselect_hyperslab(memspace,
+                                 H5S_SELECT_SET,
+                                 offsets_out, NULL,
+                                 counts, NULL);
+
+    const size_t outoffset = 0;
+    t_atom *out = (t_atom *)malloc(((n + outoffset) * sizeof(t_atom)));
+    
+    switch(class)
+    {
+    case H5T_FLOAT:
+    {
+        switch(size)
+        {
+        case 4:
+        {
+            switch(order)
+            {
+            case H5T_ORDER_LE:
+            {
+                float *d = (float *)calloc(n, sizeof(float));
+                status = H5Dread(dataset,
+                                 H5T_IEEE_F32LE,
+                                 memspace,
+                                 dataspace,
+                                 H5P_DEFAULT,
+                                 (void *)d);
+                
+                for(int i = 0; i < n; i++)
+                {
+                    atom_setfloat(out + i + outoffset, d[i]);
+                }
+            }
+            break;
+            case H5T_ORDER_BE:
+            {
+
+            }
+            break;
+            default:
+            {
+
+            }
+            break;
+            }
+        }
+        break;
+        case 8:
+        {
+            switch(order)
+            {
+            case H5T_ORDER_LE:
+            {
+                double *d = (double *)calloc(n, sizeof(double));
+                status = H5Dread(dataset,
+                                 H5T_IEEE_F64LE,
+                                 memspace,
+                                 dataspace,
+                                 H5P_DEFAULT,
+                                 (void *)d);
+            }
+            break;
+            case H5T_ORDER_BE:
+            {
+
+            }
+            break;
+            default:
+            {
+
+            }
+            break;
+            }
+        }
+        break;
+        }
+        
+    }
+    break;
+    }
+    outlet_anything(x->outlets[H5FILE_OUTLET_MAIN], ps_hyperslab,
+                    n + outoffset, out);
+    H5Sclose(memspace);
+    H5Sclose(dataspace);
+    H5Sclose(datatype);
+    H5Sclose(dataset);
+}
+
+static void h5file_getDatasetInfo(h5file *x, const char *name)
+{
+    hid_t file = x->fileid;
+    hid_t dataset = H5Dopen2(file, name, H5P_DEFAULT);
+    if(dataset < 0)
+    {
+        object_error((t_object *)x,
+                     "couldn't open dataset %s",
+                     name);
+        return;
+    }
+    
+    hid_t datatype = H5Dget_type(dataset);
+    if(datatype < 0)
+    {
+        object_error((t_object *)x,
+                     "couldn't open datatype for dataset %s",
+                     name);
+        H5Sclose(dataset);
+        return;
+    }
+    
+    H5T_class_t class = H5Tget_class(datatype);
+    H5T_order_t order = H5Tget_order(datatype);
+    size_t size = H5Tget_size(datatype);
+
+    hid_t dataspace = H5Dget_space(dataset);
+    if(dataspace < 0)
+    {
+        object_error((t_object *)x,
+                     "couldn't open dataspace for dataset %s",
+                     name);
+        H5Sclose(datatype);
+        H5Sclose(dataset);
+        return;
+    }
+    int rank = H5Sget_simple_extent_ndims(dataspace);
+    hsize_t dims[rank];
+    int status_n = H5Sget_simple_extent_dims(dataspace, dims, NULL);
+
+    int n = 5 + rank;
+    t_atom out[n];
+    atom_setsym(out, gensym(name));
+    switch(class)
+    {
+    case H5T_FLOAT:
+        atom_setsym(out + 1, ps_floatingpoint);
+        break;
+    default:
+        break;
+    }
+    switch(order)
+    {
+    case H5T_ORDER_LE:
+        atom_setsym(out + 2, ps_orderle);
+        break;
+    case H5T_ORDER_BE:
+        atom_setsym(out + 2, ps_orderbe);
+        break;
+    default:
+        break;
+    }
+    atom_setlong(out + 3, size);
+    atom_setlong(out + 4, rank);
+    for(int i = 0; i < rank; i++)
+    {
+        atom_setlong(out + 5 + i, dims[i]);
+    }
+    
+    outlet_anything(x->outlets[H5FILE_OUTLET_MAIN],
+                    ps_dataset,
+                    n,
+                    out);
+    H5Sclose(dataspace);
+    H5Sclose(datatype);
+    H5Sclose(dataset);
+}
+
+static herr_t h5file_opfunc(hid_t loc_id,
+                            const char *name,
+                            const H5L_info_t *info,
+                            void *operator_data)
+{
+    h5file *x = (h5file *)operator_data;
     herr_t status;
     H5O_info_t infobuf;
     status = H5Oget_info_by_name(loc_id, name, &infobuf, H5P_DEFAULT);
+    t_atom a[2];
     switch(infobuf.type)
     {
     case H5O_TYPE_GROUP:
-        post("	Group: %s\n", name);
+        /* post("	Group: %s\n", name); */
         break;
     case H5O_TYPE_DATASET:
-        post("	Dataset: %s\n", name);
+        /* post("	Dataset: %s\n", name); */
+        /* atom_setsym(a, ps_dataset); */
+        /* atom_setsym(a + 1, gensym(name)); */
+        /* outlet_anything(x->outlets[H5FILE_OUTLET_MAIN], */
+        /*                 ps_info, 2, a); */
+        h5file_getDatasetInfo(x, name);
         break;
     case H5O_TYPE_NAMED_DATATYPE:
-        post("	Datatype: %s\n", name);
+        /* post("	Datatype: %s\n", name); */
         break;
     default:
-        post("	Unknown: %s\n", name);
+        /* post("	Unknown: %s\n", name); */
+        break;
     }
     return 0;
 }
 
-void h5info_bang(h5file *x)
+static void h5file_info(h5file *x)
 {
     hid_t f = (hid_t)x->fileid;
     if(!f)
@@ -62,24 +356,24 @@ void h5info_bang(h5file *x)
                                    H5_ITER_NATIVE,
                                    NULL,
                                    h5file_opfunc,
-                                   NULL);
+                                   (void *)x);
     }
 }
 
-void h5file_reset(h5file *x)
+static void h5file_reset(h5file *x)
 {
     x->filename = NULL;
     x->filepath = NULL;
     x->fileid = 0;
 }
 
-void h5file_doclose(h5file *x)
+static void h5file_doclose(h5file *x)
 {
     H5Fclose(x->fileid);
     h5file_reset(x);
 }
 
-void h5file_close(h5file *x)
+static void h5file_close(h5file *x)
 {
     if(x->fileid)
     {
@@ -90,7 +384,7 @@ void h5file_close(h5file *x)
 	defer(x, (method)h5file_doclose, NULL, 0, 0L);
 }
 
-void h5file_doopen(h5file *x, t_symbol *s, long ac, t_atom *av)
+static void h5file_doopen(h5file *x, t_symbol *s, long ac, t_atom *av)
 {
 	short path;
 	char ps[MAX_PATH_CHARS];
@@ -139,20 +433,20 @@ void h5file_doopen(h5file *x, t_symbol *s, long ac, t_atom *av)
         x->fileid = fid;
     }
 
-	outlet_bang(x->outlets[H5FILE_OUTLET_DONEOPENING]);
+	outlet_bang(x->outlets[H5FILE_OUTLET_INFO]);
 }
 
-void h5file_open(h5file *x, t_symbol *s)
+static void h5file_open(h5file *x, t_symbol *s)
 {
 	defer(x, (method)h5file_doopen, s, 0, 0L);
 }
 
-void h5file_free(h5file *x)
+static void h5file_free(h5file *x)
 {
     h5file_doclose(x);
 }
 
-void h5file_assist(h5file *x, void *b, long m, long a, char *s)
+static void h5file_assist(h5file *x, void *b, long m, long a, char *s)
 {
 	if(m==1)
     {
@@ -205,7 +499,7 @@ void h5file_assist(h5file *x, void *b, long m, long a, char *s)
 /*     return 0; */
 /* } */
 
-void *h5file_new(t_symbol *sym, long ac, t_atom *av)
+static void *h5file_new(t_symbol *sym, long ac, t_atom *av)
 {
 	h5file *x = object_alloc(h5file_class);
     if(x == NULL)
@@ -218,7 +512,8 @@ void *h5file_new(t_symbol *sym, long ac, t_atom *av)
     /* { */
     /*     x->name_mangled = h5max_mangleName(x->name); */
     /* } */
-    x->outlets[H5FILE_OUTLET_DONEOPENING] = bangout((t_object *)x);
+    x->outlets[H5FILE_OUTLET_INFO] = outlet_new((t_object *)x, NULL);
+    x->outlets[H5FILE_OUTLET_MAIN] = outlet_new((t_object *)x, NULL);
     if(ac)
     {
         if(atom_gettype(av) != A_SYM)
@@ -242,7 +537,8 @@ void ext_main(void *r)
                            (short)sizeof(h5file),
                            0L, A_GIMME, 0);
 
-    class_addmethod(c, (method)h5info_bang, "bang", 0);
+    class_addmethod(c, (method)h5file_info, "info", 0);
+    class_addmethod(c, (method)h5file_getHyperslab, "gethyperslab", A_GIMME, 0);
 	class_addmethod(c, (method)h5file_open, "open", A_DEFSYM, 0);
     class_addmethod(c, (method)h5file_close, "close", 0);
 	class_addmethod(c, (method)h5file_assist,	"assist", A_CANT, 0);
@@ -256,4 +552,10 @@ void ext_main(void *r)
 	h5file_class = c;
     
 	ps_emptystring = gensym("");
+    ps_dataset = gensym("dataset");
+    ps_group = gensym("group");
+    ps_floatingpoint = gensym("floating point");
+    ps_orderle = gensym("little endian");
+    ps_orderbe = gensym("big endian");
+    ps_hyperslab = gensym("hyperslab");
 }
